8f0b8bbb74eb798db79f0446b1ea1d3278942fc7
Preethan <90035197+PPreethan@users.noreply.github.com>
2025-03-13 15:59:12 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,7 @@ def MavenBuild()
 {
   sh 'mvn package'
 }
+def depoy(Jobname,ipaddress,context_path)
+{
+    sh 'scp /var/lib/jenkins/workspace/${Jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${context_path}.war'
+}
