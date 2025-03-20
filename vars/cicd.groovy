c1a6c7ae07ec9d51b6394102eace1ae7f653b1f4
Preethan <90035197+PPreethan@users.noreply.github.com>
2025-03-20 12:43:23 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,3 +11,7 @@ def deploy(environment, server, context)
 {
   sh "scp /var/lib/jenkins/workspace/${environment}/webapp/target/webapp.war ubuntu@${server}:/var/lib/tomcat10/webapps/${context}.war"
 }
+def RunSelenium(Jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${Jobname}/testing.jar"
+}
